9a64e07c132758a9dc563639b91f34b0e87633ab
Guilherme Podolak <podolak12345@gmail.com>
2022-08-30 09:54:16 -0300

    requisito Header, problema no MapStaeteToProps

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = { expenses: PropTypes.arrayOf(PropTypes.string) }.isRequired;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
