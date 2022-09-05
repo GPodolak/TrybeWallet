e2eb55206f877a2c0b9e6724aaefdfe2bfb62bb8
Guilherme Podolak <podolak12345@gmail.com>
2022-09-04 23:24:27 -0300

    requisito 7 falta debugar

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,31 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 export default class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
+          <tbody>
+            {expenses.map((expense) => {
+              const expenseValue = Number(expense.value);
+              const cambio = Number(expense.exchangeRates[expense.currency].ask);
+              const cambioName = expense.exchangeRates[expense.currency].name;
+              console.log(expense.exchangeRates[expense.currency].name);
+              return (
+                <tr key={ expense.id }>
+                  <td>{expense.description}</td>
+                  <td>{expense.tag}</td>
+                  <td>{expense.method}</td>
+                  <td>{Number(expenseValue).toFixed(2)}</td>
+                  <td>{cambioName}</td>
+                  <td>{Number(cambio).toFixed(2)}</td>
+                  <td>{Number(cambio * expenseValue).toFixed(2)}</td>
+                  <td>Real</td>
+              </tr>);
+            })}
+          </tbody>
           <tr>
             <th>Descrição</th>
             <th>Tag</th>
@@ -19,3 +42,8 @@ export default class Table extends Component {
     );
   }
 }
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+Table.propTypes = { expenses: PropTypes.arrayOf(PropTypes.string) }.isRequired;
+export default connect(mapStateToProps)(Table);
